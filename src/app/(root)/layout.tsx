dd3ed1ac2debb720951fd460de05a2e1c4cefa97
scottnguyen0412 <scottnguyen0412@gmail.com>
2024-03-06 20:41:11 +0700

    Responsive mobile navigation

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,15 @@
+import MobileNav from '@/components/shared/MobileNav';
 import Sidebar from '@/components/shared/Sidebar';
 import React from 'react'
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <main className='root'>
+        {/* For PC */}
         <Sidebar/>
+
+        {/* For Mobile Navigation */}
+        <MobileNav/>
         <div className='root-container'>
             <div className='wrapper'>
                 {children}
